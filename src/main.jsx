a7189459ff2a804ff247e752254d8a3e724759c1
AliulhaQ07 <azii7264@gmail.com>
2024-11-05 18:15:36 +0500

    done some changes to Contact.jsx component
    add _redirects file to solve the netlify loading error.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from "./App.jsx"; // Use App as the main layout
 import "./index.css";
 import Products from "./pages/Products.jsx";
@@ -8,42 +8,33 @@ import Sales from "./pages/Sales.jsx";
 import Contact from "./pages/Contact.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Home from "./pages/Home.jsx";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "./dev/index.js";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/", // Root route
-    element: <App />, // Use App as the layout
-    errorElement: <NotFound />, // Display if route is not found
-    children: [
-      {
-        index: true, // Set this as the default route (Home page)
-        element: <Home />, // Render Home component when at "/"
-      },
-      {
-        path: "products", // /products route
-        element: <Products />,
-      },
-      {
-        path: "sales", // /sales route
-        element: <Sales />,
-      },
-      {
-        path: "contact", // /contact route
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter([{
+  path: "/", // Root route
+  element: <App/>, // Use App as the layout
+  errorElement: <NotFound/>, // Display if route is not found
+  children: [{
+    index: true, // Set this as the default route (Home page)
+    element: <Home/>, // Render Home component when at "/"
+  }, {
+    path: "products", // /products route
+    element: <Products/>,
+  }, {
+    path: "sales", // /sales route
+    element: <Sales/>,
+  }, {
+    path: "contact", // /contact route
+    element: <Contact/>,
+  },],
+},]);
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <DevSupport
-      ComponentPreviews={ComponentPreviews}
-      useInitialHook={useInitial}
-    >
-      <RouterProvider router={router} />
-    </DevSupport>
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<StrictMode>
+  <DevSupport
+    ComponentPreviews={ComponentPreviews}
+    useInitialHook={useInitial}
+  >
+    <RouterProvider router={router}/>
+  </DevSupport>
+</StrictMode>,);
